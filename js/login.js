cc950deb061bc3259827f0fca90313d9b566047f
Shreeshail <shreeshailv@cybage.com>
2022-02-07 17:38:40 +0530

    login feature added

---
+++ js/login.js
@@ -0,0 +1,48 @@
+function login()
+{
+    var loginValid;
+    //console.log("hello")
+    var emailFormInput = document.getElementById("email").value;
+    var passwordFormInput = document.getElementById("password").value;
+
+    console.log(emailFormInput+"     "+passwordFormInput);
+    fetch('http://localhost:3000/login')
+            .then(resp => resp.text())
+            .then(data => {
+                    alert(data)
+                    const jsonData = JSON.parse(data);
+                    alert(jsonData[1].email);
+
+                    for(var i = 0; i<jsonData.length; i++)
+                    {
+                        //alert(jsonData[i].email+"   "+emailFormInput)
+                        if(emailFormInput  == jsonData[i].email && passwordFormInput  == jsonData[i].password)
+                        {
+                            
+                             loginValid = 1 ; 
+                             
+                             break;
+                        }
+                        else
+                        {
+                            loginValid = 0;
+                            
+                        }
+                    }
+
+                    if(loginValid==1)
+                    {
+                        alert("success")
+                        window.location.replace("http://www.w3schools.com");
+                        
+
+                    }
+                    else
+                    {
+                        alert("please check email id or password");
+                        
+                    }
+                }
+            );
+
+}
